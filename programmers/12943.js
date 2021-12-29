1695ecc5722269aa4c3ce3c2755eaf2ef38d6d92
bam <juhyun19981129@gmail.com>
2021-12-29 16:26:37 +0900

    12944, 12943 추가

---
+++ programmers/12943.js
@@ -0,0 +1,15 @@
+function solution(num) {
+    let answer = 0;
+
+    while(num !== 1) {
+        num % 2 === 0 ? num /= 2: num = num * 3 + 1;
+        answer++;
+    }
+
+    if (answer < 500) {
+        return answer;
+    }
+    else {
+        return -1;
+    }
+}
---
+++ programmers/12944.js
@@ -0,0 +1,9 @@
+function solution(arr) {
+    let answer = 0;
+
+    for (let value of arr) {
+        answer += value;
+    }
+
+    return answer/arr.length;
+}
